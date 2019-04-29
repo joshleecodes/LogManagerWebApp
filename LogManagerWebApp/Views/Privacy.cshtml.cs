196d9a50b58de4f0cb5371d90e91084471f79111
Josh Lee <joshlee.dev@gmail.com>
2019-04-29 15:47:11 +0100

    Add files via upload

---
+++ LogManagerWebApp/Views/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogManagerWebApp.Views
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
