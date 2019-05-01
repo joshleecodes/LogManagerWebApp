1ce3651f6f456568aa3172daab1ac5efc160953d
Josh Lee <joshlee.dev@gmail.com>
2019-05-01 16:30:57 +0100

    V0.1
    Corrected 404 error caused by incorrect routing, was due to renaming the pages folder. Despite updating relevant routing to reflect a change, it seems this needs to be left as "Pages" in order for the code behind the scenes to execute correctly.
    Shared UI elements such as the navbar and titlebar have been created.

---
+++ LogManagerWebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogManagerWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
--- LogManagerWebApp/Startup.cs
+++ LogManagerWebApp/Startup.cs
@@ -31,7 +31,6 @@ namespace LogManagerWebApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
